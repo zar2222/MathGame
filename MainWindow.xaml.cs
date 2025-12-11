b29508df1cf59f8486a02323e438ca0af8ebddcc
Better <misazarubin24@gmail.com>
2025-12-11 17:07:52 +0300

    version 0.0.2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,7 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Metrics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using System.Windows.Threading;
 namespace MathGame
 {
     /// <summary>
@@ -16,16 +9,46 @@ namespace MathGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        public int counter;
+        private float Max = 1000;
+
         public MainWindow()
         {
             
             InitializeComponent();
-            SetUpGame();
-        }
 
-        private void SetUpGame()
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
+            SetUpGame();
+            
+
+		}
+
+        private void Timer_Tick(object sender, EventArgs e)
         {
-            List<string> animalList = new List<string>()
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0");
+            if (Count.Text == "8")
+            {
+                timer.Stop();
+                
+            }
+        }
+        
+       
+
+		private void SetUpGame()
+		{
+
+			
+
+			foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())  
+            {
+                textBlock.Visibility = Visibility.Visible;
+            }
+            List<string> AnimalsRep = new List<string>()
             {
                 "🐵", "🐵",
                 "🦏", "🦏",
@@ -37,13 +60,73 @@ namespace MathGame
                 "🐑", "🐑"
             };
             Random random = new Random();
+            int count = 0;
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                int index = random.Next(animalList.Count);
-                string nextEmoji = animalList[index];
-                textBlock.Text = nextEmoji;
-                animalList.RemoveAt(index);
+                
+                int index = random.Next(AnimalsRep.Count);
+                string nextEmoji = AnimalsRep[index];
+                textBlock.Text = nextEmoji; 
+                AnimalsRep.RemoveAt(index);
+                count++;
+                if (count == 16)
+                {
+                    break;
+                }
+            }
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            Count.Text = "0";
+            counter = 0;
+        }
+        TextBlock lastTextgBlockClicked;
+        bool findingMath = false;
+        
+        private void TextBlock_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            TextBlock Count = (TextBlock)mainGrid.FindName("Count");
+
+            TextBlock textBlock = sender as TextBlock;
+            
+            if (findingMath == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextgBlockClicked = textBlock;
+                findingMath = true;
+            }
+            else if (textBlock.Text == lastTextgBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMath = false;
+                counter++;
+                Count.Text = counter.ToString();
+                
+
+            }
+            else
+            {
+                lastTextgBlockClicked.Visibility = Visibility.Visible;
+                findingMath = false;
             }
         }
+        
+        private void TimeTextBlock_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+	        
+	        if (Count.Text == "8")
+	        {
+		        TextBlock BestTime = (TextBlock)mainGrid.FindName("BestTime");
+		        if (float.Parse(timeTextBlock.Text) <= Max)
+		        {
+			        Max = float.Parse(timeTextBlock.Text);
+			        BestTime.Text = Max.ToString();
+
+		        }
+
+		        SetUpGame();
+	        }
+
+
+        } 
     }
 }
